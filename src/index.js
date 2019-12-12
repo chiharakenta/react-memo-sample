200f0aeb5125dfff7312ed2e6b3b2a8530deb7e7
chiharakenta <chiharakenta0519@gmail.com>
2019-12-12 17:59:44 +0900

    Revert "complete memo app"
    This reverts commit 0a887e8684c69e962a622f4e628e286d02a19db3.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import MemoStore from './memo/Store';
+import * as serviceWorker from './serviceWorker';
 
-// 表示をレンダリング
-ReactDom.render(
-  <Provider store={MemoStore}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
