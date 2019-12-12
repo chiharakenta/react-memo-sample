0f71b74dfadf26738048edf5b9803d3a630dd170
chiharakenta <chiharakenta0519@gmail.com>
2019-12-12 17:07:43 +0900

    persist only memo data using blacklist and whitelist

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ import MemoStore, { memoReducer } from './memo/Store';
 // Redux Persistの設定
 const persistConfig = {
   key: 'memo',
-  storage
+  storage: storage,
+  blacklist: ['message', 'mode', 'fdata'],
+  whitelist: ['data']
 };
 
 // パーシストレデューサーの作成
